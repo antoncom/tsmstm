abdb9553f1717028a3a3893cdd55b48160670677
Anton Gavrilyuk <amigoalt@gmail.com>
2023-10-16 11:49:43 +0300

    Initial port to Neoway N725.

--- src/app.lua
+++ src/app.lua
@@ -24,40 +24,46 @@ local tsmstm = {}
 tsmstm.conn = nil                   -- Ubus connection
 tsmstm.fds = nil                    -- File descriptor
 tsmstm.fds_ev = nil                 -- Event loop descriptor
-tsmstm.device = "/dev/ttyS1"        -- STM32 port
+--tsmstm.device = "/dev/ttyS1"        -- STM32 port
 tsmstm.answer = ""                  -- Answer message from STM32
 
 
 function tsmstm:init()
-	if not tsmstm.fds then
-        local initcom = string.format("stty -F %s 1000000", tsmstm.device)
-        sys.exec(initcom)
+	-- if not tsmstm.fds then
+    --     local initcom = string.format("stty -F %s 1000000", tsmstm.device)
+    --     sys.exec(initcom)
 
-		tsmstm.fds = F.open(tsmstm.device, F.O_RDONLY + F.O_NONBLOCK)
-		tsmstm.conn = ubus.connect()
-		if not tsmstm.conn then
-			error("Failed to connect to ubus in 'tsmstm' module")
-		end
-	end
+	-- 	tsmstm.fds = F.open(tsmstm.device, F.O_RDONLY + F.O_NONBLOCK)
+	-- 	tsmstm.conn = ubus.connect()
+	-- 	if not tsmstm.conn then
+	-- 		error("Failed to connect to ubus in 'tsmstm' module")
+	-- 	end
+	-- end
+
+    tsmstm.conn = ubus.connect()
+    if not tsmstm.conn then
+        error("Failed to connect to ubus in 'tsmstm' module")
+    end
+
 end
 
 function tsmstm:poll()
-    if not tsmstm.fds_ev then
-        tsmstm.fds_ev = uloop.fd_add(tsmstm.fds, function(ufd, events)
-            local message_from_stm = ""
-            local ubus_response = {}
+    -- if not tsmstm.fds_ev then
+    --     tsmstm.fds_ev = uloop.fd_add(tsmstm.fds, function(ufd, events)
+    --         local message_from_stm = ""
+    --         local ubus_response = {}
 
-            message_from_stm, err, errcode = U.read(tsmstm.fds, 1024)
-			if_debug("", "STM", "POLL", tostring(message_from_stm), "[tsmstm/app.lua]: .. err = ".. tostring(err) .. "  errcode = " ..tostring(errcode))
+    --         message_from_stm, err, errcode = U.read(tsmstm.fds, 1024)
+	-- 		if_debug("", "STM", "POLL", tostring(message_from_stm), "[tsmstm/app.lua]: .. err = ".. tostring(err) .. "  errcode = " ..tostring(errcode))
 
-            if message_from_stm then
-                tsmstm.answer = tsmstm.answer .. message_from_stm
-            else
-                tsmstm.answer = "ERROR"
-            end
+    --         if message_from_stm then
+    --             tsmstm.answer = tsmstm.answer .. message_from_stm
+    --         else
+    --             tsmstm.answer = "ERROR"
+    --         end
 
-        end, uloop.ULOOP_READ)
-    end
+    --     end, uloop.ULOOP_READ)
+    -- end
 end
 
 function tsmstm:make_ubus()
@@ -70,7 +76,7 @@ function tsmstm:make_ubus()
                         if msg["command"] then
 							tsmstm.answer = ""
 							comm = string.format('echo "%s" > %s', msg["command"], tsmstm.device)
-                            stdout = sys.exec(comm)
+                            -- stdout = sys.exec(comm)
 							if_debug("", "UBUS", "ASK", comm, "[tsmstm/app.lua]: UBUS call: tsmodem.stm send method.")
                         end
                         local def_req = tsmstm.conn:defer_request(req)
